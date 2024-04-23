b071152527589a1c7627866d676a57a7a1736816
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-04-23 19:44:38 +0530

    Add import statements for requests and tqdm in textual_script.py

--- textual_script.py
+++ textual_script.py
@@ -1,8 +1,10 @@
+import requests
+from tqdm import tqdm  #For progress bar
 from textual.app import App
 
 class ArchiveDownloader(App):
     async def on_mount(self):
-        pass
+        self.title = "archive.org downloader"
 
 if __name__ == "__main__":
     app = ArchiveDownloader()
