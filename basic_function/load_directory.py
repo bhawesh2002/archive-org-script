91072201d5e26ef269e01888287273bd2bbbd228
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-16 09:02:41 +0530

    rename files

--- basic_function/load_directory.py
+++ basic_function/load_directory.py
@@ -1,6 +1,6 @@
 import json
 
-def load_directory_struct(file_path):
+def load_directory(file_path):
     with open(file_path, "r") as file:
         directory_struct = json.load(file)
     return directory_struct
