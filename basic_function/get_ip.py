2be382ea5e21ee35e6631d283cbc9f9f2d710d15
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-16 09:11:58 +0530

    rename to get_ip from get_in

--- basic_function/get_ip.py
+++ basic_function/get_ip.py
@@ -1,7 +1,7 @@
 import curses
 
 # Prompts the user for input with the specified message and returns the stripped input.
-def get_in(stdscr):
+def get_ip(stdscr):
     # Prompt the user to enter a link
     stdscr.addstr(1,0,"Paste the link here: ", curses.color_pair(4))
     stdscr.refresh()
