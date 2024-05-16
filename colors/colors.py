c90c5818baaac14ca5052e93aeaa36e873c089bd
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-16 08:40:31 +0530

    seperate color initialization into seperate fille.

---
+++ colors/colors.py
@@ -0,0 +1,10 @@
+import curses
+
+# Initialize color pairs for curses
+def init_colors():
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK)
