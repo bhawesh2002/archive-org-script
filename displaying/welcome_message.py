d0f440c64d64be8130734f92ceed4ea6049b9639
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-17 22:44:06 +0530

    refactor: Update welcome_message function to display ASCII art using pyfiglet

--- displaying/welcome_message.py
+++ displaying/welcome_message.py
@@ -1,8 +1,10 @@
 import curses
 from colors.app_colors import init_colors
+import pyfiglet
 
-def welcome_message(stdscr):
+def welcome_message(stdscr,welcome_message=""):
     init_colors()
-    stdscr.addstr("archive.org downloader", curses.color_pair(1) | curses.A_BOLD)
+    ascii_art = pyfiglet.figlet_format(welcome_message)
+    stdscr.addstr(ascii_art, curses.color_pair(1) | curses.A_BOLD)
     stdscr.refresh()
     stdscr.getch()
