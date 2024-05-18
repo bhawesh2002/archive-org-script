639a0cc851cd294f9671f0235a2b2e1477ec96ad
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-18 10:24:00 +0530

    refactor: Update error message variable name in error_messages.py

--- error_messages/error_messages.py
+++ error_messages/error_messages.py
@@ -1,1 +1,2 @@
-resize_window_error = "Please resize the window to a larger size."
+#error message displayed when terminal window is smaller than content displayed
+resize_window_err_msg = "Please resize the window to a larger size." 
