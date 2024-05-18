c1566a415d44079898270f75350b92f0c746da47
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-18 10:43:18 +0530

    refactor: Update welcome_message function to handle exceptions and raise them to be caught in main.py

--- displaying/welcome_message.py
+++ displaying/welcome_message.py
@@ -1,10 +1,13 @@
 import curses
 from colors.app_colors import init_colors
-import pyfiglet
+import pyfiglet #for creating ASCII art
 
 def welcome_message(stdscr,welcome_message=""):
-    init_colors()
-    ascii_art = pyfiglet.figlet_format(welcome_message)
-    stdscr.addstr(ascii_art, curses.color_pair(1) | curses.A_BOLD)
-    stdscr.refresh()
-    stdscr.getch()
+    try:    
+        init_colors()
+        ascii_art = pyfiglet.figlet_format(welcome_message)
+        stdscr.addstr(ascii_art, curses.color_pair(1) | curses.A_BOLD)
+        stdscr.refresh()
+        stdscr.getch()
+    except Exception as e:
+        raise e #raise the exception to be caught in main.py
