10f4e2bd8e5184d3e9ffbd792b53282974193290
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-18 10:43:11 +0530

    comment the code

--- colors/app_colors.py
+++ colors/app_colors.py
@@ -3,8 +3,8 @@ import curses
 # Initialize color pairs for curses
 def init_colors():
     curses.start_color()
-    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-    curses.init_pair(4, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK) # Blue
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK) # Red
+    curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)# Yellow
+    curses.init_pair(4, curses.COLOR_CYAN, curses.COLOR_BLACK)# Cyan
+    curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK)# Green
