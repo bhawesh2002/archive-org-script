0158c175304559576422f013aa804606a75a69c5
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-19 08:24:38 +0530

    refactor: Update welcome_message function to center ASCII art on the screen

--- displaying/welcome_message.py
+++ displaying/welcome_message.py
@@ -6,7 +6,13 @@ def welcome_message(stdscr,welcome_message=""):
     try:    
         init_colors()
         ascii_art = pyfiglet.figlet_format(welcome_message)
-        stdscr.addstr(ascii_art, curses.color_pair(1) | curses.A_BOLD)
-        stdscr.refresh()
+        height, width = stdscr.getmaxyx()
+        # Calculate the position to center the text
+        start_y = height // 2 - len(ascii_art.splitlines()) // 2
+        start_x = width // 2 - max(len(line) for line in ascii_art.splitlines()) // 2
+
+         # Display the ASCII art
+        for i, line in enumerate(ascii_art.splitlines()):
+            stdscr.addstr(start_y + i, start_x, line, curses.color_pair(4)| curses.A_BOLD)
     except Exception as e:
         raise e #raise the exception to be caught in main.py
