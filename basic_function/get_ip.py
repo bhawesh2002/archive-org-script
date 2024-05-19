4a8b19ce7d8027d8f8cd4f085d417434d8aba2c8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-19 09:25:03 +0530

    refactor: Improve user input handling in get_ip.py

--- basic_function/get_ip.py
+++ basic_function/get_ip.py
@@ -2,9 +2,19 @@ import curses
 
 # Prompts the user for input with the specified message and returns the stripped input.
 def get_ip(stdscr):
-    # Prompt the user to enter a link
-    stdscr.addstr(1,0,"Paste the link here: ", curses.color_pair(4))
+    
+    stdscr.addstr(1,0,"Paste the link here: ", curses.color_pair(3)) # Prompt the user to enter a link
     stdscr.refresh()
-    # Enable keypad mode to recognize arrow keys
-    stdscr.keypad(True)
-    return stdscr.getstr().decode('utf-8').strip()
+
+    curses.echo() # Enable echoing of characters for the user to see the input
+    curses.curs_set(1) # Show the cursor
+    stdscr.keypad(True) # Enable keypad mode to recognize arrow keys
+
+    # Get the link from the user
+    link = stdscr.getstr().decode('utf-8').strip()
+
+    curses.noecho() # Disable echoing of characters
+    curses.curs_set(0) # Hide the cursor
+    stdscr.keypad(False) # Disable keypad mode
+
+    return link # Return the link entered by the user
