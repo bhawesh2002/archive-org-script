1fd5a29dc4ba352cfcff9a6f9a344c597603b203
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-19 10:03:54 +0530

    refactor: Create construct_link function to construct download links

--- basic_function/validate_link.py
+++ basic_function/validate_link.py
@@ -1,3 +1,15 @@
 # Checks if the link follows the format of an archive.org download directory link.
-def validate_link(link):
-    return "https://archive.org/download/" in link
+def construct_link(link):
+    # If the link is directory identifier only, add the necessary prefix
+    if "https://archive.org/" not in link:
+        link = f"https://archive.org/dwonload/{link}"
+        return link
+    elif "https://archive.org/details/" in link:
+        link = link.replace("https://archive.org/details/", "https://archive.org/download/")
+        return link
+    elif "https://archive.org/download/" in link:
+        return link
+
+# def validate_link(link):
+#     return "https://archive.org/download/" in link
+
