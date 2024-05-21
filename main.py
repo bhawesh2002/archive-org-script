2257045f1302f4aba2817c9f2a8975923a46e597
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-21 18:05:48 +0530

    Refactor main.py to use correct color pair for exit message

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main(stdscr):
     #handle keyboard interrupt
     except KeyboardInterrupt as e:
         stdscr.clear() #clear the screen
-        stdscr.addstr(height//2, (width - len("Keyboad Interrupt: Exiting"))//2, "Keyboad Interrupt: Exiting", curses.color_pair(4) | curses.A_BOLD) #display the exit message
+        stdscr.addstr(height//2, (width - len("Keyboad Interrupt: Exiting"))//2, "Keyboad Interrupt: Exiting", curses.color_pair(3) | curses.A_BOLD) #display the exit message
         stdscr.refresh()
         time.sleep(0.5)
         exit(0)
