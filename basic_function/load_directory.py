482639f7de7ac66f49cbde843ed79fff0d3df3d1
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-21 13:58:47 +0530

    Refactor: Download metadata files in load_directory.py

--- basic_function/load_directory.py
+++ basic_function/load_directory.py
@@ -1,4 +1,5 @@
-import os # for creating directories for downloaded files
+import curses
+import os
 import requests # for downloading the file
 
 def get_metadata_size(identifier):
@@ -24,22 +25,34 @@ def create_download_folder(identifier):
     os.makedirs(download_folder_path, exist_ok=True)
     return download_folder_path
 
-# def download_metadata_files(identifier):
-    # download_folder_path = create_download_folder()
-    # identifier_folder = os.path.join(download_folder_path, identifier)
-    # os.mkdir(identifier_folder)
-    # files_xml_size, meta_xml_size = get_metadata_size(identifier)
-    # try:
-        # meta_xml = response = requests.get(f"https://archive.org/download/{identifier}/{identifier}_meta.xml", stream=True)
-        # if meta_xml.status_code == 200:
-            # meta_xml_file_path = os.path.join(identifier_folder, f"{identifier}_meta.xml")
-            # with open(meta_xml_file_path, 'wb') as f:
-                # f.write(response.content)
-        # files_xml = response = requests.get(f"https://archive.org/download/{identifier}/{identifier}_files.xml", stream=True)
-        # if files_xml.status_code == 200:
-            # files_xml_file_path = os.path.join(identifier_folder, f"{identifier}_files.xml")
-            # with open(files_xml_file_path, 'wb') as f:
-                # f.write(response.content)
-                # 
-    # except requests.RequestException as e:
-        # # raise e
+# Download the metadata files
+def download_metadata_files(stdscr,identifier):
+    download_folder_path = create_download_folder(identifier) #create the download folder
+    attempts = 0 # Number of attempts to download the files
+    status = False # Status of the download
+    try: 
+        while not status:
+            if attempts >= 3:
+                stdscr.addstr(f"Error: Unable to download {identifier}_meta.xml and {identifier}_files.xml\n", curses.color_pair(2))
+                break
+            attempts += 1
+            # Download the *_meta.xml file
+            meta_response = requests.get(f"https://archive.org/download/{identifier}/{identifier}_meta.xml", stream=True)
+            if meta_response.status_code == 200:
+                with open(f"{download_folder_path}/{identifier}_meta.xml", 'wb') as f:
+                    f.write(meta_response.content)
+                    f.close()
+            else:
+                stdscr.addstr(f"Error: Unable to download {identifier}_meta.xml\n", curses.color_pair(2))
+            # Download the *_files.xml file
+            files_response = requests.get(f"https://archive.org/download/{identifier}/{identifier}_files.xml", stream=True)
+            if files_response.status_code == 200:
+                with open(f"{download_folder_path}/{identifier}_files.xml", 'wb') as f:
+                    f.write(files_response.content)
+                    f.close()
+            else:
+                stdscr.addstr(f"Error: Unable to download {identifier}_files.xml\n", curses.color_pair(2))
+    except requests.RequestException as e:
+        raise (f"download_metadata_files:{e}")
+    
+    return status # Return the status of the download
