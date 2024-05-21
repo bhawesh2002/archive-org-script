5a529f44e866f9a5af20aa0125999cccc5d14b98
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-21 18:06:53 +0530

    Refactor error_messages.py to add keyboard interrupt message

--- error_messages/error_messages.py
+++ error_messages/error_messages.py
@@ -1,2 +1,5 @@
 #error message displayed when terminal window is smaller than content displayed
 resize_window_err_msg = "Please resize the window to a larger size." 
+
+#error message displayed when the user exits the program using the keyboard interrupt
+keyboard_interrupt_msg = "Keyboad Interrupt: Exiting" 
