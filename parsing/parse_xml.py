63ac17e0b58e5be7c01729628d94a78ede945a4a
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-24 09:43:07 +0530

    Refactor parse_xml function to include identifier parameter and update XML file path

--- parsing/parse_xml.py
+++ parsing/parse_xml.py
@@ -1,8 +1,11 @@
 import xml.etree.ElementTree as ET # Used for parsing XML files
 import json # Used for dumping the parsed data to a JSON file
-
+import os # Used for appending the path to the XML and JSON file
+from constants import DOWNLOAD_FOLDER_PATH
 # Parses the XML file (assumed to be _files.xml) and dumps it to json file.
-def parse_xml(xml_file):
+def parse_xml(identifier):
+    
+    xml_file = os.path.join(DOWNLOAD_FOLDER_PATH, identifier, "metadata",f"{identifier}_files.xml") # Path to the XML file
     tree = ET.parse(xml_file)
     root = tree.getroot()
     file_tree = {}
