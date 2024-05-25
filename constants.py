a35025754c9d5b982ca8de4c3172aa50eddff7a2
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-25 09:37:32 +0530

    Refactor constants.py to update CONTROLS constant with correct key for help message

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ import os
 
 PROGRAM_NAME = "Archive.org Dwonloader" #program name
 DOWNLOAD_FOLDER_PATH = os.path.join(os.getcwd(), "Archives") #download folder path
-CONTROLS = "Controls: [Arrow Keys] to navigate, [Enter] to select, [Esc] to exit, F1 for help" #controls message
+CONTROLS = "Controls: [Arrow Keys] to navigate, [Enter] to select, [Esc] to exit, 'h' for help" #controls message
 #help message
 HELP_TEXT = [
         "Help",
@@ -17,5 +17,5 @@ HELP_TEXT = [
         "d: Download the selected item",
         "r: Refresh the display",
         "c: Clear the search results",
-        "F1: Display this help message",
+        "h: Display this help message",
     ]
