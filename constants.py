9564bce5078bc4a53448dbe3301d8e788781ab5c
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-25 09:16:46 +0530

    Refactor constants.py to add CONTROLS constant

--- constants.py
+++ constants.py
@@ -2,3 +2,4 @@ import os
 
 PROGRAM_NAME = "Archive.org Dwonloader" #program name
 DOWNLOAD_FOLDER_PATH = os.path.join(os.getcwd(), "Archives") #download folder path
+CONTROLS = "Controls: [Arrow Keys] to navigate, [Enter] to select, [Esc] to exit, F1 for help" #controls message
