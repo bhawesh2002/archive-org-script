fc0c37b6f7ddee206ce4f472a930d9da6271a6b7
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-26 11:45:28 +0530

    Refactor: Modify display_help function and use try-except block

--- displaying/display_help.py
+++ displaying/display_help.py
@@ -3,7 +3,11 @@ from colors.app_colors import init_colors
 from constants import HELP_TEXT
 
 def display_help(stdscr):
-    stdscr.addstr(0 , 0, "Help", curses.color_pair(6) | curses.A_BOLD)
-    for i, line in enumerate(HELP_TEXT):
-        stdscr.addstr(0 + i, 0, line, curses.color_pair(5))
-    return
+    try: 
+        stdscr.addstr(0,0,"Help\n", curses.color_pair(5) | curses.A_BOLD)
+        stdscr.addstr(1,0,"----------------------\n", curses.color_pair(5) | curses.A_BOLD)
+        y = stdscr.getyx()[0] #get the position of cursor
+        for i,line in enumerate(HELP_TEXT):
+            stdscr.addstr(y + i, 0, line, curses.color_pair(5))
+    except Exception as e:
+        raise e
