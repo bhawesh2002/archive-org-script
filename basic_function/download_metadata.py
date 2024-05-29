eb8752f6cc7e21a457efe513ca89d97b8fe4a137
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-29 12:44:03 +0530

    Refactor download_metadata.py to display the download progress

--- basic_function/download_metadata.py
+++ basic_function/download_metadata.py
@@ -36,30 +36,48 @@ def download_metadata_files(stdscr,identifier,queue,):
     attempts = 0 # Number of attempts to download the files
     status = False # Status of the download
     try: 
+        files_xml_size, meta_xml_size = get_metadata_size(identifier) # Get the size of the metadata files
+        y = stdscr.getyx()[0]
         while not status:
             if attempts >= 3:
-                y = stdscr.getyx()[0]
                 stdscr.addstr(y,0,f"Error: Maximum Attempts Reached\n", curses.color_pair(4))
                 break
+            
+            # Increment the number of attempts
             attempts += 1
+
             # Download the *_meta.xml file
             meta_response = requests.get(f"https://archive.org/download/{identifier}/{identifier}_meta.xml", stream=True)
             if meta_response.status_code == 200:
-                with open(f"{metadata_folder_path}/{identifier}_meta.xml", 'wb') as f:
-                    f.write(meta_response.content)
-                    f.close()
+                meta_downloaded_size = 0
+                with open(f"{metadata_folder_path}/{identifier}_meta.xml", 'wb') as meta_xml:
+                    for chunk in meta_response.iter_content(chunk_size=10): # Write the file in 10 byte chunks
+                        if chunk:
+                            meta_xml.write(chunk)
+                            meta_downloaded_size += len(chunk)
+                            stdscr.addstr(y+1,0,f"{identifier}_meta.xml: {meta_downloaded_size}/{meta_xml_size} bytes", curses.color_pair(4) | curses.A_BOLD)
+                            stdscr.refresh()
+                    meta_xml.close() # Close the file
             else:
                 continue # If the download fails, try again
+
             # Download the *_files.xml file
             files_response = requests.get(f"https://archive.org/download/{identifier}/{identifier}_files.xml", stream=True)
             if files_response.status_code == 200:
-                with open(f"{metadata_folder_path}/{identifier}_files.xml", 'wb') as f:
-                    f.write(files_response.content)
-                    f.close()
+                files_downloaded_size = 0
+                with open(f"{metadata_folder_path}/{identifier}_files.xml", 'wb') as files_xml:
+                    for chunk in files_response.iter_content(chunk_size=10): # Write the file in 10 byte chunks
+                        if chunk:
+                            files_xml.write(chunk)
+                            files_downloaded_size += len(chunk)
+                            stdscr.addstr(y+2,0,f"{identifier}_files.xml: {files_downloaded_size}/{files_xml_size} bytes", curses.color_pair(4) | curses.A_BOLD)
+                            stdscr.refresh()
+                    files_xml.close() # Close the file
             else:
                 continue # If the download fails, try again
 
             status = True # If the download is successful, set status to True
+        stdscr.deleteln()
     except requests.HTTPError as http_err: # Handle HTTP errors
         print(f'HTTP error occurred: {http_err}')
         raise http_err
@@ -110,6 +128,7 @@ def download_metadata(stdscr,identifier, queue):
         # If the download is successful, display a success message
         if result: 
             stdscr.deleteln()
+            stdscr.deleteln()
             stdscr.addstr(y,0,"Metadata Downloaded Successfully\n", curses.color_pair(5) | curses.A_BOLD)
             time.sleep(0.5)
             stdscr.refresh()
@@ -117,6 +136,7 @@ def download_metadata(stdscr,identifier, queue):
         # If the download fails, display an error message and exit the program
         else:
             y = stdscr.getyx()[0]
+            stdscr.deleteln()
             stdscr.deleteln()
             stdscr.addstr(y,0,f"Metadata Download Failed\n", curses.color_pair(3) | curses.A_BOLD)
             stdscr.addstr(y,0,"Exiting...\n", curses.color_pair(3) | curses.A_BOLD)
