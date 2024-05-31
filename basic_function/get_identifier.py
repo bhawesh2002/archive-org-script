c640745dbbad47a55d977de03dfc89d268738840
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-05-31 19:46:36 +0530

    Refactor get_identifier.py to improve user input handling

--- basic_function/get_identifier.py
+++ basic_function/get_identifier.py
@@ -3,8 +3,8 @@ import requests #for making HTTP requests
 
 # Get the identifier from the download link
 def get_identifier(stdscr):
-    download_link = user_input(stdscr) #get the download link from the user
-    identifier = download_link.split("/")[-1] #extract the identifier from the link
+    user_ip = user_input(stdscr) #get the download link from the user
+    identifier = user_ip.split("/")[-1] #extract the identifier from the link
     return identifier
 
 #User input function to get the download link from the user
@@ -17,27 +17,11 @@ def user_input(stdscr):
     stdscr.keypad(True) # Enable keypad mode to recognize arrow keys
 
     # Get the link from the user
-    link = stdscr.getstr().decode('utf-8').strip()
+    user_ip = stdscr.getstr().decode('utf-8').strip()
 
     curses.noecho() # Disable echoing of characters
     curses.curs_set(0) # Hide the cursor
     stdscr.keypad(False) # Disable keypad mode
     stdscr.refresh()
 
-    download_link = construct_download_link(link) # Construct the download link
-
-    return download_link
-
-# Checks if the link follows the format of an archive.org download directory link.
-def construct_download_link(link):
-    # If the link is directory identifier only, add the necessary prefix
-    if "https://archive.org/" not in link:
-        link = f"https://archive.org/download/{link}"
-        return link
-    # If the link is a details page, replace the details with download
-    elif "https://archive.org/details/" in link:
-        link = link.replace("https://archive.org/details/", "https://archive.org/download/")
-        return link
-    # If the link is a download link, return the link
-    elif "https://archive.org/download/" in link:
-        return link
+    return user_ip
