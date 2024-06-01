7e1d6d2d49d8e408921f02110c2daccb0782a0b7
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-01 21:19:34 +0530

    Refactor file_browser to remove unnessary code

--- displaying/build_display_env.py
+++ displaying/build_display_env.py
@@ -7,9 +7,7 @@ def file_browser(stdscr, identifier):
     init_colors() 
     stdscr.clear()
     height, width = stdscr.getmaxyx() #get the height and width of the terminal
-    x = (width - len(identifier))//2 #center the "File Browser" text
     stdscr.addstr(0 , (width - len("File Browser"))//2, "File Browser", curses.color_pair(6) | curses.A_BOLD) #display the program name at the top center
     y = height -1
     stdscr.addstr(height -1, 0, CONTROLS, curses.color_pair(4)) #display the controls at the bottom of the screen
-    return x , y
     
