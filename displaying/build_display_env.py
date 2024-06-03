17d9ba68c869879f4a12e67deecd53a3f3af799d
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-03 09:57:09 +0530

    Refactor file_browser function to use a new window for displaying the file browser

--- displaying/build_display_env.py
+++ displaying/build_display_env.py
@@ -5,9 +5,10 @@ from constants import CONTROLS
 def file_browser(stdscr):
     curses.curs_set(0) #hide the cursor
     init_colors() 
-    stdscr.clear()
-    height, width = stdscr.getmaxyx() #get the height and width of the terminal
-    x = (width - len("File Browser"))//2
-    y = height -1
-    stdscr.addstr(0 , x, "File Browser", curses.color_pair(6) | curses.A_BOLD) #display the program name at the top center
-    stdscr.addstr(y, 0, CONTROLS, curses.color_pair(4)) #display the controls at the bottom of the screen
+    height,width = stdscr.getmaxyx()
+    browser_win = curses.newwin(height,width , 0, 0) #create a new window relative to the stdandard screen(stdscr)
+    browser_win.border() #draw a border around the window
+    browser_ht, browser_wt = browser_win.getmaxyx() #get the height and width of the window
+    browser_win.addstr(0,(width - len("  File Browser  ")) // 2,"  File Browser  ", curses.color_pair(4) | curses.A_BOLD) #display the title of the window
+    browser_win.addstr(browser_ht-1, 1 , f" {CONTROLS} ", curses.color_pair(5) | curses.A_BOLD) #display the controls
+    browser_win.refresh() #refresh the window
