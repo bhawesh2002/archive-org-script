ba9b96d39ec3fe73924b5da94433849795cc73a8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-03 10:26:06 +0530

    Refactor display_help function to use a new window for displaying the help message

--- displaying/display_help.py
+++ displaying/display_help.py
@@ -2,13 +2,18 @@ import curses
 from colors.app_colors import init_colors
 from constants import HELP_TEXT
 
-def display_help(stdscr):
+def display_help(help_win_ht, help_win_wt, y,x):
     try: 
         init_colors()
-        stdscr.addstr(0,0,"Help\n", curses.color_pair(5) | curses.A_BOLD)
-        stdscr.addstr(1,0,"----------------------\n", curses.color_pair(5) | curses.A_BOLD)
-        y = stdscr.getyx()[0] #get the position of cursor
+        help_win = curses.newwin(help_win_ht, help_win_wt, y, x) #create a new window for displaying help
+
+        #display help contents
         for i,line in enumerate(HELP_TEXT):
-            stdscr.addstr(y + i, 0, line, curses.color_pair(5))
+            help_win.addstr(y + i, 0, f"   {line}", curses.color_pair(5))
+        #draw the border
+        help_win.border()
+        #display the help message
+        help_win.addstr(0, (help_win_wt - len("  Help  "))//2, "  Help  ", curses.color_pair(5) | curses.A_BOLD)
+        help_win.refresh()
     except Exception as e:
         raise e
