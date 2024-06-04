295be8107607dca6081301798bf070461d3231d1
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-04 09:52:32 +0530

    refactor: use main_win instead of browser_win

--- displaying/file_browser.py
+++ displaying/file_browser.py
@@ -8,19 +8,19 @@ def file_browser(stdscr):
         curses.curs_set(0) #hide the cursor
         init_colors() 
         height,width = stdscr.getmaxyx()
-        browser_win = curses.newwin(height,width , 0, 0) #create a new window relative to the stdandard screen(stdscr)
-        browser_win.border() #draw a border around the window
-        browser_ht, browser_wt = browser_win.getmaxyx() #get the height and width of the window
-        browser_win.addstr(0,(width - len("  File Browser  ")) // 2,"  File Browser  ", curses.color_pair(4) | curses.A_BOLD) #display the title of the window
-        browser_win.addstr(browser_ht-1, 1 , f" {CONTROLS} ", curses.color_pair(5) | curses.A_BOLD) #display the controls
-        browser_win.refresh() #refresh the window
+        main_win = curses.newwin(height,width , 0, 0) #create a new window relative to the stdandard screen(stdscr)
+        main_win.border() #draw a border around the window
+        main_ht, main_wt = main_win.getmaxyx() #get the height and width of the window
+        main_win.addstr(0,(width - len("  File Browser  ")) // 2,"  File Browser  ", curses.color_pair(4) | curses.A_BOLD) #display the title of the window
+        main_win.addstr(main_ht-1, 1 , f" {CONTROLS} ", curses.color_pair(5) | curses.A_BOLD) #display the controls
+        main_win.refresh() #refresh the window
         help_required = False #initialize the help status
         while True:
-            b_key = browser_win.getch() #get the key pressed
-            browser_win.refresh()
+            b_key = main_win.getch() #get the key pressed
+            main_win.refresh()
             if b_key == ord('h'):
                 help_required = not help_required #toggle the help message
-                display_help(browser_ht - 2, (browser_wt)//3, 1, browser_wt - ((browser_wt//3) + 2),help_required) #display the help message
+                display_help(main_ht - 2, (main_wt)//3, 1, main_wt - ((main_wt//3) + 2),help_required) #display the help message
             if b_key == ord('\033'):
                 break #exit the file browser
     except Exception as e:
