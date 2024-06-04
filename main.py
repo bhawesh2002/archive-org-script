77b1de5671191b51ea1c32bf4a97c2a17c837f9d
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-04 08:37:27 +0530

    comment the code for better understanding

--- main.py
+++ main.py
@@ -26,13 +26,13 @@ def main(stdscr):
         welcome_message(stdscr, PROGRAM_NAME) #display the welcome message        
         stdscr.addstr(PROGRAM_NAME, curses.color_pair(6) | curses.A_BOLD) #display the program name
         download_status = False #initialize the download status
-        while not download_status:
+        while not download_status: #loop until the download is successful
             identifier = get_identifier(stdscr) #get the identifier
             download_status = download_metadata(stdscr, identifier, queue.Queue()) #download the metadata files
         parse_xml(identifier) #parse the xml file
         time.sleep(1) #wait for 1 second
-        stdscr.clear()
-        stdscr.refresh()
+        stdscr.clear() #clear the screen
+        stdscr.refresh() #refresh the screen
         file_browser(stdscr) #open the file browser
         stdscr.refresh() #refresh the screen
     #handle keyboard interrupt
@@ -40,19 +40,18 @@ def main(stdscr):
         stdscr.clear() #clear the screen
         stdscr.addstr(height//2, (width - len(keyboard_interrupt_msg))//2, keyboard_interrupt_msg, curses.color_pair(3) | curses.A_BOLD) #display the exit message
         stdscr.refresh()
-        time.sleep(0.5)
+        time.sleep(0.5) #wait for 0.5 seconds
         exit(0)
     #handle other errors
     except Exception as e:
         stdscr.clear() 
         x = (width -len(str(e)))//2 #center the error message
         stdscr.addstr(height//2,x, f"{e}", curses.color_pair(2) | curses.A_BOLD) #display the error message
-        # stdscr.addstr(height//2 + 1,(width - len(resize_window_err_msg))//2, f"{resize_window_err_msg}") #display the solution for the error
         stdscr.refresh()
 
     # Logic for the key press
     while True:
-        key = stdscr.getch()
+        key = stdscr.getch() #wait for the user to press a key
         stdscr.refresh()
         if key == ord('\033'):  # '\033' for 'Esc' key in ASCII
             exit(0)
