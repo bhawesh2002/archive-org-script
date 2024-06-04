30987964a8540b107b2860e655f6ef8a4123e538
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-04 09:30:19 +0530

    Refactor main.py to remove unused imports

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from basic_function.download_metadata import download_metadata #import load_dire
 import queue # for passing messages between threads
 from parsing.parse_xml import parse_xml #import parse xml
 from displaying.file_browser import file_browser #import build_display_env to build the display environment
-from displaying.display_help import display_help #import display_directory_struct to display the directory structure
 # import os #for creating directories for dwonaloded files
 # from basic_function.get_directory_identifier import get_directory_identifier #import get_directory_identifier
 # from download_metadata.download_metadata import download_metadata_files #import download metadata
