b79fa0b2af76ba440d144f1ff8f70f26709cc4b1
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-05 09:28:01 +0530

    refactor: Update browser_window function to display filetree items in the file browser window

--- displaying/file_browser.py
+++ displaying/file_browser.py
@@ -27,10 +27,25 @@ def file_browser(stdscr, identifier_name,filetree, selected_files, current_opt=0
     except Exception as e:
         raise e
 
-def browser_window(main_win,filetree,current_opt,selected_files,):
+def browser_window(main_win,filetree,current_opt,selected_files):
     try:
         main_ht, main_wt = main_win.getmaxyx() #get the height and width of the window
-        browser_win = curses.newwin(main_ht - 2, main_wt-4, 1, 2) #create a new window for displaying help
+        browser_win = curses.newwin(main_ht - 2, main_wt-5, 1, 2) #create a new window for displaying help
+        height, width = browser_win.getmaxyx()
+        max_visible_lines = height-1 #max number of items that can be displayed on the screen
+        for idx,(key,value) in enumerate(filetree.items()):
+            if idx > max_visible_lines:
+                continue
+            if idx == current_opt:
+                browser_win.attron(curses.A_REVERSE)
+                browser_win.addstr(idx,0,key)
+                browser_win.attroff(curses.A_REVERSE)
+            else:
+                browser_win.addstr(idx,0,key)
+            if isinstance(value,dict): #if value is a dictionary object i.e a folder with nested folder/files
+                browser_win.addstr(idx,len(key)+1,"-->")
+            elif isinstance(value,str): #else if child_folder is a string(i.e, size of file) which is associated with key which is name of file
+                browser_win.addstr(idx,len(key)+1,f"({value})")
         browser_win.refresh()
     except Exception as e:
         raise e
