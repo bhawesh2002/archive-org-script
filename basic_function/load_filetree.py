6461f83cf021dafa3fe675c4d365fb1d62eea381
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-05 09:27:46 +0530

    comment the code

--- basic_function/load_filetree.py
+++ basic_function/load_filetree.py
@@ -7,4 +7,4 @@ def load_filetree(identifier):
     """Loads the filetree.json file for the given identifier."""
     filetree_path = os.path.join(DOWNLOAD_FOLDER_PATH, identifier, "metadata",f"{identifier}_filetree.json") # Path to the filetree.json file
     with open(filetree_path, 'r') as filetree:
-        return json.load(filetree)
+        return json.load(filetree) # Load the filetree.json file and return the data
