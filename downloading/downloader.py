112cb9d7c17ea48e897606affb2fe922444a671f
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-16 20:22:08 +0530

    refactor: create_content_folder and call download_file function to downlaod file

--- downloading/downloader.py
+++ downloading/downloader.py
@@ -1,6 +1,14 @@
 import curses
+import os
 from basic_function.load_download_links import load_download_links
 from colors.app_colors import init_colors
+from constants import DOWNLOAD_FOLDER_PATH
+from downloading.download_file import download_file
+
+def create_content_folder(identifier):
+    content_folder_path = os.path.join(DOWNLOAD_FOLDER_PATH, identifier, "content")
+    os.makedirs(content_folder_path, exist_ok=True)
+    return content_folder_path
 
 def downloader(stdscr,identifier):
     try:
@@ -12,5 +20,11 @@ def downloader(stdscr,identifier):
         downloader_win.addstr(0,width//2 - len(" Downloader ")," Downloader ",curses.color_pair(4) | curses.A_BOLD) #display the title of the window
         downloader_win.refresh() #refresh the window
         links = load_download_links(identifier) #load the download links
+        content_folder_path = create_content_folder(identifier)
+        for link in links:
+            download_file(url=link,downloader_win=downloader_win,download_path=content_folder_path)
+        downloader_win.addstr(height//2,(width-len("Download Complete"))//2,"Download Complete", curses.color_pair(5) | curses.A_BOLD) #display the download complete message
+        downloader_win.addstr((height//2)+1,(width-len("Press any Key to EXIT"))//2,"Press any Key to EXIT", curses.color_pair(8)) #display the download complete message
+        downloader_win.getch()
     except Exception as e:
         raise e
