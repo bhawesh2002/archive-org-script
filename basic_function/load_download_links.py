72e14fbc3236a968f314e465faa436e1bb8a01f8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-16 19:39:27 +0530

    refactor: Update load_download_links.py to read download links from file line by line

--- basic_function/load_download_links.py
+++ basic_function/load_download_links.py
@@ -6,5 +6,6 @@ def load_download_links(identifier):
     metadata_folder = os.path.join(DOWNLOAD_FOLDER_PATH,identifier,"metadata")
     download_links = []
     with open(f'{metadata_folder}/download_links.txt','r') as f:
-        download_links = f.read().split("\n")
+        for line in f:
+            download_links.append(line.strip())
     return download_links
