0b5ab2625c196b604889b72bd5d3022d28355785
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-06-16 20:21:29 +0530

    refactor: Update download_file function to improve error handling and progress display

--- downloading/download_file.py
+++ downloading/download_file.py
@@ -3,35 +3,28 @@ import requests
 
 from constants import DOWNLOAD_FOLDER_PATH
 
-def download_file(url, identifier, filename):
+def download_file(url,downloader_win,download_path):
     HEADERS = {
             'Accept-Encoding': 'identity' # Avoid content encoding when fetching the headers
     }
-    status = False # Set the status to False by default
+    filename = url.split("/")[-1]
     try:
-        file = requests.get(url,stream=True,headers=HEADERS)
-        if file.status_code == 200:
-            file_size = file.headers.get('Content-Length', None)
+        response = requests.get(url,stream=True, headers=HEADERS)
+        if response.status_code == 200:
+            filesize = response.headers.get('Content-Length', None)
             bytes_downloaded = 0
-            with open(f"{DOWNLOAD_FOLDER_PATH}/{identifier}/'content'/{filename}", 'wb') as f:
-                for chunk in file.iter_content(chunk_size=10): # Write the file in 10 byte chunks
+            with open(f"{download_path}/{filename}", 'wb') as f:
+                for chunk in response.iter_content(chunk_size=1024):
                     if chunk:
                         f.write(chunk)
                         bytes_downloaded += len(chunk)
-                
-    except requests.HTTPError as http_err: # Handle HTTP errors
-        print(f'HTTP error occurred: {http_err}')
-        raise http_err
-    except ConnectionError as conn_err: # Handle connection errors
-        print(f'Connection error occurred: {conn_err}')
-        raise conn_err
-    except requests.Timeout as timeout_err: # Handle timeout errors
-        print(f'Timeout error occurred: {timeout_err}')
-        raise timeout_err
-    except requests.RequestException as req_err: # Handle request errors
-        print(f'Request error occurred: {req_err}')
-        raise req_err
-    except Exception as err: # Handle other errors
-        print(f'An error occurred: {err}')
-        raise err
-
+                        downloader_win.addstr(2,2,f'Downloading {filename} - {bytes_downloaded}/{filesize}')
+                        downloader_win.clrtoeol()
+                        downloader_win.refresh()
+                f.close()
+    except requests.HTTPError as http_err:
+        downloader_win.addstr(2,2,f'HTTP error occurred: {http_err}')
+        downloader_win.refresh()
+    except Exception as e:
+        downloader_win.addstr(2,2,f'An error occurred: {e}')
+        downloader_win.refresh()
