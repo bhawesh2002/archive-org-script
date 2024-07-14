425c634e9f9e935f766e1b9ef25b9e60d9b9c961
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-07-14 21:37:54 +0530

    refactor: Update controls message and help text
    This commit updates the controls message and help text in the `constants.py` file. The controls message now includes the updated key mappings for navigation, selection, and downloading. The help text has been modified to reflect the new key mappings as well. These changes improve the usability and clarity of the program's controls and help information.

--- constants.py
+++ constants.py
@@ -2,17 +2,20 @@ import os
 
 PROGRAM_NAME = "Archive.org Dwonloader" #program name
 DOWNLOAD_FOLDER_PATH = os.path.join(os.getcwd(), "Archives") #download folder path
-CONTROLS = "Controls: [Arrow Keys] to navigate, [Space] to select, [Esc] to exit, 'h' to toggle help message" #controls message
+CONTROLS = "Controls: [WSAD Keys] to navigate, [SPACEBAR] to select, [ENTER] to download, [H] to display help message, [ESC] to quit" #controls message
+EXTENSION_SEL_CONTROLS = "[WSAD keys] to navigate, [SPACEBAR] to select the extension"
+EXTENSION_CONFIRMATION_MESSAGE = "[ENTER] to confirm selection,[BACKSPACE] to go back"
 #help message
 HELP_TEXT = [
-        "Arrow keys: Navigate the file browser",
-        "Enter: Open the selected item",
-        "Space: Selete a file or folder",
-        "Backspace: Go Back",
-        "Esc: Quit the program",
-        "s: Search for an item",
-        "d: Download the selected item",
-        "r: Refresh the display",
-        "c: Clear the search results",
-        "h: Toggle help message",
+        "W: Navigate up",
+        "S: Navigate down",
+        "A: Go back to the previous folder",
+        "D: Open the highlighted folder",
+        "SPACEBAR: Select the highlighted file/folder",
+        "NUM 1: Select all files/folders",
+        "NUM 0: Deselect all files/folders",
+        "NUM 2: Open Extensions Selection Menu",
+        "ENTER: Download the selected files/folders",
+        "H: Display help message",
+        "ESC: Quit the program",
     ]
